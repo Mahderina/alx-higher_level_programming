b7fcbf0d6e17f2086cd6fa591c19cf86ab255510
Mahderina <111346013+Mahderina@users.noreply.github.com>
2023-01-11 00:09:21 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def append_write(filename="", text=""):
     """"append write"""
     with open(filename, mode="a", encoding="utf-8") as fd:
         fd.write(text)
-    return len(text
+    return len(text)
